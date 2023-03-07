286e950e728c6d73064d81c4ffa5d9d10ac77945
sujal <sujalagrawalgondia@gmail.com>
2023-03-07 12:03:46 +0530

    added requitements.txt and versel.json

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pw)82v2373@l!7w4=dd-&=u1$ekqkxkcp4s4q-abo%ibcm0+9z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
